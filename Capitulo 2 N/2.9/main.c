fac9668de4b8ff0135b5b9c18300fe64c76cf606
Nicovf23 <114188244+Nicovf23@users.noreply.github.com>
2022-11-03 14:49:28 -0400

    Add files via upload

---
+++ Capitulo 2 N/2.9/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main(void)
+{
+float RES;
+int R, T, Q;
+printf("Ingrese los valores de R, T y Q: ");
+scanf("%d %d %d", &R, &T, &Q);
+if (RES < 820)
+printf("\nR = %d\tT = %d\t Q = %d", R, T, Q);
+}
